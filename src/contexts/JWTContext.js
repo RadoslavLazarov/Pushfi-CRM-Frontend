5c0bd83bc89db2afc032cabf72f7550d3a30316b
Radoslav Lazarov <radoslav.lazarov@flatrocktech.com>
2022-08-12 00:42:41 +0300

    add navigationFallback config for Azure

--- src/contexts/JWTContext.js
+++ src/contexts/JWTContext.js
@@ -22,7 +22,7 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   baseUrl = 'https://localhost:44375';
 } else {
   // production code
-  baseUrl = 'https://pushfi-api.azurewebsites.net/';
+  baseUrl = 'https://pushfi-api.azurewebsites.net';
 }
 
 // constant
